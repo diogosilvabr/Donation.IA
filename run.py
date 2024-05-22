4eee7b595748dca014d126d47207048492a3e1a6
dioguera <60168982+diogosilvabr@users.noreply.github.com>
2024-05-22 08:43:45 -0300

    Update run.py

--- run.py
+++ run.py
@@ -4,12 +4,6 @@ from flask_cors import CORS
 
 app = create_app()
 
-# Configuração do Swagger
-SWAGGER_URL = '/swagger'
-API_URL = '/static/swagger.json'
-swaggerui_blueprint = get_swaggerui_blueprint(SWAGGER_URL, API_URL, config={'app_name': "Donation.IA"})
-app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
-
 # Adicionar suporte a CORS
 CORS(app)
 
