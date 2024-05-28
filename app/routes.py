ddbbccbd1bc522b060680428ff710f4b4dd70826
diogosilvabr <di.silvargt@gmail.com>
2024-05-27 22:31:43 -0300

    correções no feedback

--- app/routes.py
+++ app/routes.py
@@ -11,9 +11,6 @@ modelo, vetorizador = carregarModelo('ml/modelo_inappropriate.pkl', 'ml/vetoriza
 @api_bp.route('/analyze-text', methods=['POST'])
 def analyze_text():
     data = request.json
-    if 'text' not in data:
-        return jsonify({'error': 'Missing text parameter'}), 400
-
     texto = data.get('text', '')
     texto_limpo = preprocessarTexto(texto)
     vetor = vetorizador.transform([texto_limpo])
@@ -26,19 +23,18 @@ def analyze_text():
 
     return jsonify({'inapropriado': is_inappropriate})
 
-# Endpoint para adicionar feedback manualmente
 @api_bp.route('/add-feedback', methods=['POST'])
 def add_feedback():
     data = request.json
-    if 'text' not in data or 'inappropriate' not in data:
-        return jsonify({'error': 'Missing text or inappropriate parameter'}), 400
+    texto = data.get('text', '')
+    inappropriate = data.get('inappropriate', 0)
 
-    text = data['text']
-    inappropriate = data['inappropriate']
+    # Preprocessar o texto
+    texto_limpo = preprocessarTexto(texto)
 
-    # Adicionar o feedback ao arquivo CSV
-    with open('data/feedback_atualizado.csv', mode='a', newline='', encoding='utf-8') as file:
-        writer = csv.writer(file)
-        writer.writerow([text, inappropriate])
+    # Salvar o feedback no arquivo CSV
+    with open('data/feedback.csv', 'a', newline='', encoding='utf-8') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow([texto, texto_limpo, inappropriate])
 
-    return jsonify({'message': 'Feedback added successfully'}), 200
+    return jsonify({'message': 'Feedback added successfully'})
