e2a5fd05aced33cd13e9c7a262e44762c55e8713
dioguera <60168982+diogosilvabr@users.noreply.github.com>
2024-06-03 15:14:32 -0300

    Update modelo.py

--- ml/modelo.py
+++ ml/modelo.py
@@ -1,6 +1,10 @@
 import joblib
 
+# Define a função para carregar o modelo de machine learning e o vetorizador
 def carregarModelo(caminho_modelo, caminho_vetorizador):
+    # Carrega o modelo a partir do caminho especificado
     modelo = joblib.load(caminho_modelo)
+    # Carrega o vetorizador a partir do caminho especificado
     vetorizador = joblib.load(caminho_vetorizador)
+    # Retorna o modelo e o vetorizador carregados
     return modelo, vetorizador
