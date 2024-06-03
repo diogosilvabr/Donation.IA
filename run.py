b0a61fad06c87ad5793e0408fc1f5e79431f8f64
dioguera <60168982+diogosilvabr@users.noreply.github.com>
2024-06-03 15:23:25 -0300

    Update run.py

--- run.py
+++ run.py
@@ -2,10 +2,13 @@ from flask import Flask
 from flask_cors import CORS
 from app import create_app
 
+# Cria a aplicação Flask usando a função create_app
 app = create_app()
 
-# Adicionar suporte a CORS
+# Adiciona suporte a CORS à aplicação
 CORS(app)
 
+# Se o script for executado diretamente, inicia o servidor Flask
 if __name__ == "__main__":
+    # Executa a aplicação no modo debug na porta 8888
     app.run(debug=True, port=8888)
